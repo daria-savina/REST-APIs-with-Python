bcc8dac8340223ee273e50aa6fb4cae45a1df37d
Daria Savina <dariasavinase@gmail.com>
2022-09-11 22:52:01 +0300

    first comm

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,jsonify
+from flask_restful import Api,Resource,reqparse
+
+
+app = Flask(__name__)
+api = Api()
+
+tasks = {
+    1: {"title": "Buy groceries", "description": "Milk, Cheese, Cola, Fruit"},
+    2: {"title": "Learn Python", "description": "create Resp API"}
+}
+
+parser = reqparse.RequestParser()
+parser.add_argument("title", location='form')
+
+class Main(Resource):
+    def get(self,task_id):
+        if task_id == 0:
+           return tasks
+        else:
+            return tasks[task_id]
+
+    def delete(self, task_id):
+        del tasks[task_id]
+        return tasks
+
+    def post(self,task_id):
+        tasks[task_id]  = parser.parse_args()
+        return tasks
+
+    def put(self,course_id):
+        tasks[task_id] = parser.parse_args()
+        return tasks
+
+api.add_resource(Main, "/api/tasks/<int:task_id>")
+api.init_app((app))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='127.0.0.1')
+
---
+++ front.py
@@ -0,0 +1,12 @@
+import requests
+
+headers = {'Accept': 'application/json'}
+
+# res = requests.get('http://127.0.0.1:5000/api/tasks/0')
+res = requests.put('http://127.0.0.1:5000/api/tasks/1' , {"title": "Buy groceries","description": "Tomato, Pasta,Cheese"})
+# res = requests.post('http://127.0.0.1:5000/api/tasks/3',{"title": "Go to the gym", "make you do it"})
+# res = requests.delete('http://127.0.0.1:5000/api/tasks/5',{"title": "SQL"})
+
+
+print(res.json())
+
